b8f5c0d7341dca946b5dec1518c77eed5c1887e3
William Hatfield <hatwill@seas.upenn.edu>
2025-09-11 14:38:58 -0400

    cdc

--- hw2/cdc/cdc.cpp
+++ hw2/cdc/cdc.cpp
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
+#include "stopwatch.h"
 
 #define WIN_SIZE 16
 #define PRIME 3
 #define MODULUS 256
 #define TARGET 0
+
+stopwatch cdc_time;
+stopwatch cdc_time_revised;
 
 uint64_t hash_func(unsigned char *input, unsigned int pos)
 {
@@ -62,14 +66,50 @@ void test_cdc( const char* file )
 
 	int bytes_read = fread(&buff[0],sizeof(unsigned char),file_size,fp);
 
-	cdc_revised(buff, file_size);
+	cdc_time.start();
+	cdc(buff, file_size);
+	cdc_time.stop();
+	printf("Average latency of cdc is: %lf ns.\n", cdc_time.avg_latency());
 
     free(buff);
     return;
 }
 
+void test_cdc_revised( const char* file )
+{
+	FILE* fp = fopen(file,"r" );
+	if(fp == NULL ){
+		perror("fopen error");
+		return;
+	}
+
+	fseek(fp, 0, SEEK_END); // seek to end of file
+	int file_size = ftell(fp); // get current file pointer
+	fseek(fp, 0, SEEK_SET); // seek back to beginning of file
+
+	unsigned char* buff = (unsigned char *)malloc((sizeof(unsigned char) * file_size ));	
+	if(buff == NULL)
+	{
+		perror("not enough space");
+		fclose(fp);
+		return;
+	}
+
+	int bytes_read = fread(&buff[0],sizeof(unsigned char),file_size,fp);
+
+	cdc_time_revised.start();
+	cdc_revised(buff, file_size);
+	cdc_time_revised.stop();
+	printf("Total latency of cdc_revised is: %lf ns.\n", cdc_time_revised.avg_latency());
+
+    free(buff);
+    return;
+}
+
+
 int main()
 {
 	test_cdc("prince.txt");
+	test_cdc_revised("prince.txt");
 	return 0;
 }
